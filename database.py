7d180403bfba00062ff2dfa95bbe6c9d7d103de7
DANIEL ALEJANDRO MARTINEZ <danielpandiaco2020@gmail.com>
2025-11-30 17:29:58 -0500

    Conectando PostgreSQL

--- database.py
+++ database.py
@@ -1,26 +1,30 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+from sqlalchemy.ext.declarative import declarative_base
 
-load_dotenv()
+# 1. Intentamos leer la URL de la base de datos de la Nube (Render)
+# Si no existe (porque estás en tu PC), usamos la local "torneo.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Buscamos la dirección de la base de datos en las variables de entorno
-# Si no existe (estamos en local), usamos sqlite
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./torneo.db")
-
-# Corrección para Render: Suelen dar la URL con "postgres://", pero SQLAlchemy necesita "postgresql://"
-if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
-# Configuración diferente según el motor
-if "sqlite" in SQLALCHEMY_DATABASE_URL:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+if not SQLALCHEMY_DATABASE_URL:
+    # Modo Local (Tu PC)
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./torneo.db"
+    connect_args = {"check_same_thread": False} # Necesario solo para SQLite
 else:
-    # Configuración para PostgreSQL (Nube)
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    # Modo Nube (PostgreSQL)
+    # Corrección pequeña: Render a veces da la URL con "postgres://", pero Python necesita "postgresql://"
+    if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    connect_args = {}
 
+# 2. Creamos el Motor
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
+)
+
+# 3. Creamos la Sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# 4. La Base para los modelos
 Base = declarative_base()
